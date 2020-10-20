94760992a07e95c1c1ca0c584959c49197a65aa6
Adigunaram <55024884+Adigunaram@users.noreply.github.com>
2020-10-20 20:49:09 +0700

    install express

--- server.js
+++ server.js
@@ -1,53 +1,15 @@
-// HTTP Portion
-var http = require("http");
-// Path module
-var path = require("path");
+const express = require("express");
+const morgan = require("morgan");
 
-// Using the filesystem module
-var fs = require("fs");
+const app = express();
 
-var server = http.createServer(handleRequest);
-server.listen(8080);
+app.use(express.static("public"));
+app.use(morgan("short"));
 
-console.log("Server started on port 8080");
+app.get("/", (req, res) => {
+  res.sendFile(__dirname, "/index.html");
+});
 
-function handleRequest(req, res) {
-  // What did we request?
-  var pathname = req.url;
-
-  // If blank let's ask for index.html
-  if (pathname == "/") {
-    pathname = "/index.html";
-  } else if (pathname != "/") {
-    console.log(pathname);
-  }
-
-  // Ok what's our file extension
-  var ext = path.extname(pathname);
-
-  // Map extension to file type
-  var typeExt = {
-    ".html": "text/html",
-    ".js": "text/javascript",
-    ".css": "text/css",
-  };
-  // What is it?  Default to plain text
-
-  var contentType = typeExt[ext] || "text/plain";
-
-  // User file system module
-  fs.readFile(
-    __dirname + pathname,
-    // Callback function for reading
-    function (err, data) {
-      // if there is an error
-      if (err) {
-        res.writeHead(500);
-        return res.end("Error loading " + pathname);
-      }
-      // Otherwise, send the data, the contents of the file
-      res.writeHead(200, { "Content-Type": contentType });
-      res.end(data);
-    }
-  );
-}
+app.listen(8080, () => {
+  console.log("Server is running");
+});
